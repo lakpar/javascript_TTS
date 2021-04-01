9a77885a034c790edf7908b3a62993550e222355
Laksman Paranthaman <laksman@Laksmans-MacBook-Pro.local>
2021-04-01 11:43:24 -0400

    week 2

---
+++ Javascript/Week 2/jsonObject.js
@@ -0,0 +1,12 @@
+var objJSON = {};
+console.log(objJSON);
+
+objJSON["HotelShoppingRS"] = {};
+console.log(objJSON);
+
+objJSON.HotelShoppingRS["Documents"] = {};
+console.log(objJSON);
+
+objJSON.HotelShoppingRS.Documents["Name"] = "Clarity NDC Gateway";
+objJSON.HotelShoppingRS.Documents["ReferenceVersion"] = "1.0";
+console.log(objJSON);
