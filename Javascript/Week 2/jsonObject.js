e12958ac8022f9e8812d309fe64c70b0445c7883
Laksman Paranthaman <laksman@Laksmans-MacBook-Pro.local>
2021-04-01 19:31:48 -0400

    week 2

--- Javascript/Week 2/jsonObject.js
+++ Javascript/Week 2/jsonObject.js
@@ -10,3 +10,18 @@ console.log(objJSON);
 objJSON.HotelShoppingRS.Documents["Name"] = "Clarity NDC Gateway";
 objJSON.HotelShoppingRS.Documents["ReferenceVersion"] = "1.0";
 console.log(objJSON);
+
+for (x in objJSON.HotelShoppingRS.Documents){
+  console.log(x);
+}
+
+//JSON Object iteration
+var newJSON = {};
+var garage = ["Ford F150", "BMW 320i", "Honda Fit"];
+newJSON["Garage"] = {};
+for (x = 0; x < garage.length; x++){
+  var key = "Car " + (x+1)
+  newJSON.Garage[key] = garage[x];
+}
+
+console.log(newJSON);
